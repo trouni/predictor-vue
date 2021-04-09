9a60ae0371334f785d0a74f770f64e3621f12c06
Trouni Tiet <trouni@gmail.com>
2021-04-09 20:27:39 +0900

    Force automatic logout upon 401 status code

--- src/utils/request.js
+++ src/utils/request.js
@@ -29,6 +29,9 @@ service.interceptors.response.use(
     return response
   },
   error => {
+    if (error.response.status === 401) {
+      store.dispatch('auth/logOut')
+    }
     console.warn(error)
     return Promise.reject(error)
   }
