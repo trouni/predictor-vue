cf3478d0a942faa816bb703350e1e4aeba3c4670
Trouni Tiet <trouni@gmail.com>
2021-04-09 19:34:27 +0900

    Add router and login view

---
+++ src/router/index.js
@@ -0,0 +1,90 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
+import NProgress from 'nprogress/nprogress'
+import store from '@/store'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+  base: process.env.BASE_URL,
+})
+
+// Before each route evaluates...
+router.beforeEach((routeTo, routeFrom, next) => {
+  // If this isn't an initial page load...
+  if (routeFrom.name !== null) {
+    // Start the route progress bar.
+    NProgress.start()
+  }
+
+  // Check if auth is required on this route
+  // (including nested routes).
+  const authRequired = routeTo.matched.some(route => route.meta.authRequired)
+
+  // If auth isn't required for the route, just continue.
+  if (!authRequired) return next()
+
+  // If auth is required
+  store.getters['auth/loggedIn'] ? next() : redirectToLogin()
+
+  function redirectToLogin() {
+    // Pass the original route to the login component
+    next({ name: 'login', query: { redirectFrom: routeTo.fullPath } })
+  }
+})
+
+router.beforeResolve(async (routeTo, routeFrom, next) => {
+  // Create a `beforeResolve` hook, which fires whenever
+  // `beforeRouteEnter` and `beforeRouteUpdate` would. This
+  // allows us to ensure data is fetched even when params change,
+  // but the resolved route does not. We put it in `meta` to
+  // indicate that it's a hook we created, rather than part of
+  // Vue Router (yet?).
+  try {
+    // For each matched route...
+    for (const route of routeTo.matched) {
+      await new Promise((resolve, reject) => {
+        // If a `beforeResolve` hook is defined, call it with
+        // the same arguments as the `beforeEnter` hook.
+        if (route.meta && route.meta.beforeResolve) {
+          route.meta.beforeResolve(routeTo, routeFrom, (...args) => {
+            // If the user chose to redirect...
+            if (args.length) {
+              // If redirecting to the same route we're coming from...
+              if (routeFrom.name === args[0].name) {
+                // Complete the animation of the route progress bar.
+                NProgress.done()
+              }
+              // Complete the redirect.
+              next(...args)
+              reject(new Error('Redirected'))
+            } else {
+              resolve()
+            }
+          })
+        } else {
+          // Otherwise, continue resolving the route.
+          resolve()
+        }
+      })
+    }
+    // If a `beforeResolve` hook chose to redirect, just return.
+  } catch (error) {
+    return
+  }
+
+  // If we reach this point, continue resolving the route.
+  next()
+})
+
+// When each route is finished evaluating...
+router.afterEach(() => {
+  // router.afterEach((routeTo, routeFrom) => {
+  // Complete the animation of the route progress bar.
+  NProgress.done()
+})
+
+export default router
---
+++ src/router/routes.js
@@ -0,0 +1,48 @@
+import store from '@/store'
+
+export default [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/home.vue'),
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login.vue'),
+    meta: {
+      beforeResolve(routeTo, routeFrom, next) {
+        // If the user is already logged in
+        if (store.getters['auth/loggedIn']) {
+          // Redirect to the home page instead
+          next({ name: 'home' })
+        } else {
+          // Continue to the login page
+          next()
+        }
+      },
+    },
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    meta: {
+      beforeResolve(routeTo, routeFrom, next) {
+        store.dispatch('auth/logOut')
+        const authRequiredOnPreviousRoute = routeFrom.matched.some(
+          route => route.meta.authRequired
+        )
+        // Navigate back to previous page, or home as a fallback
+        next(authRequiredOnPreviousRoute ? { name: 'home' } : { ...routeFrom })
+      },
+    },
+  },
+  {
+    path: '/path',
+    name: 'route-name',
+    // component: () => import('@/views/view-component.vue'),
+    meta: {
+      authRequired: true,
+    },
+  },
+]
