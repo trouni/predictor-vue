a386e64e9c3a47e7b8f429b35cbb44ba9fb9478e
Trouni Tiet <trouni@gmail.com>
2021-04-14 17:19:57 +0900

    Add FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
+import '@/plugins/fontawesome'
 import router from '@/router'
 import store from '@/store'
-import App from './App.vue'
+import App from '@/App.vue'
 
 // Globally register all `_base`-prefixed components
 import '@/components/_globals'
