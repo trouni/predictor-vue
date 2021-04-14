281dc6156f5a26bf871f7f11f4e7dba7890a4e2c
Trouni Tiet <trouni@gmail.com>
2021-04-14 23:16:24 +0900

    Remove comments

--- src/api/repository-factory.js
+++ src/api/repository-factory.js
@@ -10,18 +10,11 @@ const repositoriesContext = require.context(
 const repositories = {}
 
 repositoriesContext.keys().forEach(fileName => {
-  // Get the PascalCase version of the repository name
   const repositoryName = fileName
     // Remove the "./_" from the beginning
     .replace(/^\.\//, '')
     // Remove the file extension from the end
     .replace(/\.\w+$/, '')
-  // // Split up kebabs
-  // .split('-')
-  // // Upper case
-  // .map(kebab => kebab.charAt(0).toUpperCase() + kebab.slice(1))
-  // // Concatenated
-  // .join('')
 
   repositories[repositoryName] = repositoriesContext(fileName).default
 })
