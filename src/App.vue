3e1a062eac99037dc8fa3d106143a3f9b82f423a
Trouni Tiet <trouni@gmail.com>
2021-04-14 16:58:47 +0900

    Add base stylesheets

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,53 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+// Normalize default styles across browsers,
+// https://necolas.github.io/normalize.css/
+@import '~normalize.css/normalize.css';
+// Style loading bar between pages.
+// https://github.com/rstacruz/nprogress
+@import '~nprogress/nprogress.css';
+// Style variables and utilities from src/styles.
+@import '@/styles';
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+body {
+  background: $color-body-bg;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @extend %typography-small;
+}
+// ===
+// Base element styles
+// ===
+a,
+a:visited {
+  color: $color-link-text;
+}
+h1 {
+  @extend %typography-xxlarge;
+}
+h2 {
+  @extend %typography-xlarge;
+}
+h3 {
+  @extend %typography-large;
+}
+h4 {
+  @extend %typography-medium;
+}
+h5,
+h6 {
+  @extend %typography-small;
+}
+// ===
+// Vendor
+// ===
+#nprogress .bar {
+  background: $color-link-text;
 }
 </style>
