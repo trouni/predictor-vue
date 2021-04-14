f71a573dcaef9b1a2c825b746bf21c358bb3d9d9
Trouni Tiet <trouni@gmail.com>
2021-04-14 18:43:47 +0900

    Improve components to accept function attr

--- src/components/NavBarRoutes.vue
+++ src/components/NavBarRoutes.vue
@@ -14,16 +14,26 @@ export default {
   },
   // Render functions are an alternative to templates
   render(h, { props }) {
-    function getRouteTitle(route) {
-      return typeof route.title === 'function' ? route.title() : route.title
+    function getRouteAttr(route, attr) {
+      return typeof route[attr] === 'function' ? route[attr]() : route[attr]
+    }
+    function processRoute(route) {
+      Object.keys(route).forEach(
+        attr => (route[attr] = getRouteAttr(route, attr))
+      )
+      return route
     }
     // Functional components are the only components allowed
     // to return an array of children, rather than a single
     // root node.
     return props.routes.map(route => (
-      <BaseLink key={route.name} to={route} exact-active-class='active'>
+      <BaseLink
+        key={route.name}
+        to={processRoute(route)}
+        exact-active-class='active'
+      >
         <li>
-          <a>{getRouteTitle(route)}</a>
+          <a>{processRoute(route).title}</a>
         </li>
       </BaseLink>
     ))
