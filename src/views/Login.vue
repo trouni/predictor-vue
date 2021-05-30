fa40fb4147da694d4c290d59251e1fb097c37c3b
Douglas Berkley <douglasmberkley@gmail.com>
2021-05-30 13:56:41 +0900

    put the form in a tailwind container

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,39 @@
 <template>
-  <div>
-    <p v-if="register">
-      Already got an account?
-      <BaseButton @click="register = false">Log in</BaseButton>
-    </p>
-    <p v-else>
-      Need an account?
-      <BaseButton @click="register = true">Register</BaseButton>
-    </p>
-    <div>
-      <p>Email</p>
-      <p v-if="authError" v-html="authError"></p>
-      <BaseInputText
-        v-model="email"
-        label="Email"
-        name="email"
-        type="text"
-        autofocus
-        @keypress.enter="submit"
-      />
-      <p>Password</p>
-      <BaseInputText
-        id="password"
-        v-model="password"
-        label="Password"
-        name="password"
-        type="password"
-        @keypress.enter="submit"
-      />
+  <div class="flex flex-col justify-center items-center">
+    <div class="w-full md:w-6/12">
+      <p v-if="register">
+        Already got an account?
+        <BaseButton @click="register = false">Log in</BaseButton>
+      </p>
+      <p v-else>
+        Need an account?
+        <BaseButton @click="register = true">Register</BaseButton>
+      </p>
       <div>
-        <BaseButton :disabled="processingForm" @click="submit">
-          {{ register ? 'Sign up' : 'Login' }}
-        </BaseButton>
+        <p>Email</p>
+        <p v-if="authError" v-html="authError"></p>
+        <BaseInputText
+          v-model="email"
+          label="Email"
+          name="email"
+          type="text"
+          autofocus
+          @keypress.enter="submit"
+        />
+        <p>Password</p>
+        <BaseInputText
+          id="password"
+          v-model="password"
+          label="Password"
+          name="password"
+          type="password"
+          @keypress.enter="submit"
+        />
+        <div>
+          <BaseButton :disabled="processingForm" @click="submit">
+            {{ register ? 'Sign up' : 'Login' }}
+          </BaseButton>
+        </div>
       </div>
     </div>
   </div>
