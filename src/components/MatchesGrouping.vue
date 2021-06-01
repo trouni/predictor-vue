da509687574fc690e94d86f4701a1cc0e2174d4f
Trouni Tiet <trouni@gmail.com>
2021-06-01 22:31:49 +0900

    Create match groupings

---
+++ src/components/MatchesGrouping.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="matches.length" class="mb-10">
+    <h3 class="text-center uppercase">{{ title }}</h3>
+    <MatchCard
+      v-for="match in matches"
+      :key="match.id"
+      :match="match"
+      :selectable="match.status == 'upcoming'"
+    />
+  </div>
+</template>
+
+<script>
+import MatchCard from '@/components/MatchCard'
+
+export default {
+  components: { MatchCard },
+
+  props: {
+    matches: {
+      type: Array,
+      default: () => [],
+    },
+    title: String,
+  },
+}
+</script>
