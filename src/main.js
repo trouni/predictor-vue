652f64c6566128deced865ac469a42ca4ca50cc7
Trouni Tiet <trouni@gmail.com>
2021-06-02 23:53:13 +0900

    Add vue pwa

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import App from '@/App.vue'
 // Globally register all `_base`-prefixed components
 import '@/components/_globals'
 
+import './registerServiceWorker'
+
 // Don't warn about using the dev version of Vue in development.
 Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
