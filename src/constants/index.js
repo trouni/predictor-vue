d1af89389184cdb38ea8624d13a4b0cc0e700b34
Trouni Tiet <trouni@gmail.com>
2021-06-11 18:24:30 +0900

    Add Google Analytics

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,5 @@ export const config = {
   environment: process.env.NODE_ENV || 'development',
   apiURL: process.env.VUE_APP_API_URL,
   cloudName: process.env.VUE_APP_CLOUD_NAME,
+  analyticsID: process.env.VUE_APP_ANALYTICS_ID,
 }
