3282d6cb4b973c673f432b5379be5ab38a437836
Trouni Tiet <trouni@gmail.com>
2021-06-12 21:48:38 +0900

    Fix size of button text on matches page

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -14,7 +14,7 @@
           predict!
         </p>
         <BaseButton class="uppercase text-center">
-          <BaseLink :to="{ path: '/predictions' }" class="text-xl p-3">
+          <BaseLink :to="{ path: '/predictions' }" class="md:text-lg p-3">
             <BaseIcon name="arrow-circle-right" class="mr-3" />Make your
             predictions
           </BaseLink>
@@ -26,7 +26,7 @@
           upcoming matches!
         </p>
         <BaseButton class="uppercase text-center">
-          <BaseLink :to="{ path: '/predictions/edit' }" class="text-lg p-3">
+          <BaseLink :to="{ path: '/predictions/edit' }" class="md:text-lg p-3">
             <BaseIcon name="redo-alt" class="mr-3" />Update your predictions
           </BaseLink>
         </BaseButton>
