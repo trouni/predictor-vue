348e823df4898b32b8b5652552f11f8427c1ab37
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-16 13:19:00 +0900

    Added overflow scroll on the main container

--- src/views/layouts/DefaultLayout.vue
+++ src/views/layouts/DefaultLayout.vue
@@ -4,7 +4,7 @@
     <Header :title="title" :img="img">
       <component v-if="subHeader" :is="subHeader" ref="subHeader" />
     </Header>
-    <div class="flex justify-center w-full h-full">
+    <div class="flex justify-center w-full h-full overflow-scroll">
       <main
         class="bg-wrapper overflow-y-auto rounded-t-3xl flex-grow p-4 pb-12 relative max-w-screen-md"
         ref="main"
