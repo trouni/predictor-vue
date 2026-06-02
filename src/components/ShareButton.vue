575108ef5bd8b23ff5d455c08677abe9c838ba9c
Douglas Berkley <douglasmberkley@gmail.com>
2026-06-02 21:48:36 +0900

    fixed the share button as well

--- src/components/ShareButton.vue
+++ src/components/ShareButton.vue
@@ -1,10 +1,7 @@
 <template>
-  <BaseButton
-    class="visibility-hidden tooltip secondary"
-    data-tooltip="URL Copied!"
-    @click="copy($event)"
-  >
-    <BaseIcon :name="icon" /> <span class="ml-2">{{ text }}</span>
+  <BaseButton class="secondary" @click="copy">
+    <BaseIcon :name="copied ? 'check' : icon" />
+    <span class="ml-2">{{ copied ? 'URL Copied!' : text }}</span>
   </BaseButton>
 </template>
 
@@ -27,10 +24,17 @@ export default {
       required: false,
     },
   },
+  data() {
+    return {
+      copied: false,
+    }
+  },
   methods: {
-    copy(event) {
-      event.target.parentElement.classList.remove('visibility-hidden')
-      setTimeout(this.hidePopOver, 3000)
+    copy() {
+      this.copied = true
+      setTimeout(() => {
+        this.copied = false
+      }, 3000)
       const text = window.location.origin + '/join/' + this.password
       const elem = document.createElement('textarea')
       elem.value = text
@@ -39,61 +43,6 @@ export default {
       document.execCommand('copy')
       document.body.removeChild(elem)
     },
-    hidePopOver() {
-      document.querySelector('.tooltip').classList.add('visibility-hidden')
-    },
   },
 }
 </script>
-
-<style lang="scss" scoped>
-@import '@/styles';
-.tooltip {
-  position: relative;
-  cursor: pointer;
-}
-.tooltip:before,
-.tooltip:after {
-  opacity: 1;
-  pointer-events: none;
-}
-/* Position tooltip above the element */
-.tooltip:before {
-  position: absolute;
-  bottom: 150%;
-  left: 50%;
-  margin-bottom: 5px;
-  margin-left: -40px;
-  padding: 7px;
-  width: 80px;
-  -webkit-border-radius: 3px;
-  -moz-border-radius: 3px;
-  border-radius: 3px;
-  background-color: $purple;
-  color: $white;
-  content: attr(data-tooltip);
-  text-align: center;
-  font-size: 14px;
-  line-height: 1.2;
-}
-
-/* Triangle hack to make tooltip look like a speech bubble */
-.tooltip:after {
-  position: absolute;
-  bottom: 150%;
-  left: 50%;
-  margin-left: -5px;
-  width: 0;
-  border-top: 5px solid #000;
-  border-top: 5px solid hsla(0, 0%, 20%, 0.9);
-  border-right: 5px solid transparent;
-  border-left: 5px solid transparent;
-  content: ' ';
-  font-size: 0;
-  line-height: 0;
-}
-.visibility-hidden.tooltip:before,
-.visibility-hidden.tooltip:after {
-  visibility: hidden;
-}
-</style>
