ba6407878e2f8cd1221fdb734faed21b37d6763f
Douglas Berkley <douglasmberkley@gmail.com>
2026-06-02 22:04:21 +0900

    updated modal placement

--- src/components/ConfirmDelete.vue
+++ src/components/ConfirmDelete.vue
@@ -2,7 +2,7 @@
   <transition name="modal-fade">
     <div class="fixed z-10 inset-0 overflow-y-auto">
       <div
-        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+        class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
       >
         <div class="fixed inset-0 transition-opacity" aria-hidden="true">
           <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
@@ -63,7 +63,7 @@
               @click="deleteRecord"
               class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
             >
-              Delete
+              Leave
             </button>
             <button
               type="button"
